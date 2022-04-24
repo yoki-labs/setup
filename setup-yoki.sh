8273cfb7356633aa5803537cb18289ee5f4eea4a
Nico <nico.03727+github@gmail.com>
2022-04-24 15:56:33 -0400

    Update setup-yoki.sh

--- setup-yoki.sh
+++ setup-yoki.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/yoki-labs/yoki yoki
 cd yoki
 sudo rm -rf services/ scripts/ .husky/ .yarn/ .github/
-sudo rm .eslintignore .eslintrc.json .gitignore package.json README.md tsconfig.eslint.json tsconfig.json
-yarn.lock
+sudo rm .eslintignore .eslintrc.json .gitignore package.json README.md tsconfig.eslint.json tsconfig.json yarn.lock
